22ded7d8165bc109e456919434a9121a5ce7c9f9
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-06 23:08:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,6 @@ class _InputPageState extends State<InputPage> {
       body: Center(
         child: Text('Body Text'),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
