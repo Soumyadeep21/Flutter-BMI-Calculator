445915329304a768e955d7117814cb5e6c908548
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-07 01:09:41 +0530

    Update input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const activeCardColor = Color(0xff1D1F33);
+const inactiveCardColor = Color(0xff111328);
 const bottomContainerColor = Color(0xffEA1556);
 const bottomContainerHeight = 80.0;
 
