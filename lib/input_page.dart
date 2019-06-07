3b41ad670f09352432df656b0acf33251d8abbaf
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-07 21:35:37 +0530

    Update input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,6 +13,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender selectedGender;
+  int height = 180;
 
   @override
   Widget build(BuildContext context) {
@@ -78,7 +79,7 @@ class _InputPageState extends State<InputPage> {
                     textBaseline: TextBaseline.alphabetic,
                     children: <Widget>[
                       Text(
-                        '180',
+                        '$height',
                         style: kNumberStyle,
                       ),
                       Text(
@@ -86,6 +87,15 @@ class _InputPageState extends State<InputPage> {
                         style: kLabelStyle,
                       )
                     ],
+                  ),
+                  Slider(
+                    value: height.toDouble(),
+                    min: 120.0,
+                    max: 220.0,
+                    activeColor: Color(0xffeb1555),
+                    onChanged: (newValue){
+                      print(newValue);
+                    },
                   ),
                 ],
               ),
