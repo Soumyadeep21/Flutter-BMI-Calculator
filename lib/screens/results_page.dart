e0daf27d31d2fab360731b4813acea47537606c0
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-08 00:41:33 +0530

    Update results_page.dart

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -1,5 +1,6 @@
 import 'package:bmi_calculator/components/reusable_card.dart';
 import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/screens/bottom_button.dart';
 import 'package:flutter/material.dart';
 
 class ResultsPage extends StatelessWidget {
@@ -46,7 +47,13 @@ class ResultsPage extends StatelessWidget {
                 ],
               ),
             ),
-          )
+          ),
+          BottomButton(
+            buttonTitle: 'RE-CALCULATE',
+            onTap: (){
+              Navigator.of(context).pop();
+            },
+          ),
         ],
       ),
     );
