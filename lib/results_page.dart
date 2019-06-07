0d78091d89050c8801b1b3678779c4f9387cfa4f
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-07 23:50:11 +0530

    Create results_page.dart

---
+++ lib/results_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+        centerTitle: true,
+      ),
+      body: Text('HELLO'),
+    );
+  }
+}
