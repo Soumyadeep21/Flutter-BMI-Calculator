15889f767887eb7825f42a61683b36d81e7b0794
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-07 10:37:47 +0530

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const activeCardColor = Color(0xff1D1F33);
+const inactiveCardColor = Color(0xff111328);
+const bottomContainerColor = Color(0xffEA1556);
+const bottomContainerHeight = 80.0;
+
+const labelStyle = TextStyle(
+  fontSize: 18,
+  color: Color(0xff8d8e98),
+);
