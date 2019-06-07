4325c4aad0e01166b01b0d3cd87e683e3ed6c491
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-08 01:30:52 +0530

    Update calculator_BMI.dart

--- lib/calculator_BMI.dart
+++ lib/calculator_BMI.dart
@@ -8,12 +8,14 @@ class CalculatorBMI {
   CalculatorBMI({this.height, this.weight});
 
   String getBMI() {
-    _bmi = weight / pow(height/100, 2);
+    _bmi = weight / pow(height / 100, 2);
     return _bmi.toStringAsFixed(1);
   }
 
   String getResult() {
-    if (_bmi > 25)
+    if (_bmi > 30)
+      return 'Obesity';
+    else if (_bmi > 25)
       return 'Overweight';
     else if (_bmi > 18.5)
       return 'Normal';
@@ -22,7 +24,9 @@ class CalculatorBMI {
   }
 
   String getInterpretation() {
-    if (_bmi > 25)
+    if (_bmi > 30)
+      return 'Ohh No! You have much more weight than that of a normal person. Consult a doctor immediately!!';
+    else if (_bmi > 25)
       return 'Ahh! Your body weight is more than normal. Try to exercise more and maintain a healthy diet.';
     else if (_bmi > 18.5)
       return 'WOW! You have a perfect weight. Let\'s maintain this.';
